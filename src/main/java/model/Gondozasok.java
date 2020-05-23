1e811e9f00c738bead4e15a3ff3baf89585abccc
Rácz András István <randris99@gmail.com>
2020-05-23 11:28:58 +0200

    Model Gondozasok added

--- src/main/java/model/Gondozasok.java
+++ src/main/java/model/Gondozasok.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalTime;
 
@@ -17,10 +15,15 @@ import java.time.LocalTime;
 public class Gondozasok {
 
     @Id
-    private Integer gID;
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
 
-    @Column(name = "D_ID")
-    private Integer dID;
+    @Column(name = "Ellatott_Neve")
+    private String elderName;
+
+    @Column(name = "Dolgozo_Neve")
+    private String employeeName;
 
     @Column(name = "Ebed")
     private String lunch;
@@ -34,4 +37,38 @@ public class Gondozasok {
     @Column(name = "Gondozasi_Ido")
     private LocalTime careTime;
 
+    @Column(name = "Ebed_ara")
+    private Integer price;
+
+    public Integer getId() { return id; }
+
+    public void setId(Integer id) { this.id = id; }
+
+    public String getElderName() { return elderName; }
+
+    public void setElderName(String elderName) { this.elderName = elderName; }
+
+    public String getEmployeeName() { return employeeName; }
+
+    public void setEmployeeName(String employeeName) { this.employeeName = employeeName; }
+
+    public String getLunch() { return lunch; }
+
+    public void setLunch(String lunch) { this.lunch = lunch; }
+
+    public LocalDate getDate() { return date; }
+
+    public void setDate(LocalDate date) { this.date = date; }
+
+    public LocalTime getTravelTime() { return travelTime; }
+
+    public void setTravelTime(LocalTime travelTime) { this.travelTime = travelTime; }
+
+    public LocalTime getCareTime() { return careTime; }
+
+    public void setCareTime(LocalTime careTime) { this.careTime = careTime; }
+
+    public Integer getPrice() { return price; }
+
+    public void setPrice(Integer price) { this.price = price; }
 }
